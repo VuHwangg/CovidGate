681b92c8277e38fe494904f323f89a62044474ea
Hoàng Chu Anh Vũ <bonneybonny30082002@gmail.com>
2022-01-14 22:25:52 +0700

    Complete Contact Menu

--- js/script.js
+++ js/script.js
@@ -9,3 +9,38 @@ if (toastTrigger) {
     toast.show()
   })
 }
+
+// Modal Send Email - Cửa sổ send email
+var exampleModal = document.getElementById('exampleModal')
+exampleModal.addEventListener('show.bs.modal', function (event) {
+  // Button that triggered the modal
+  var button = event.relatedTarget
+  // Extract info from data-bs-* attributes
+  var recipient = button.getAttribute('data-bs-whatever')
+  // If necessary, you could initiate an AJAX request here
+  // and then do the updating in a callback.
+  //
+  // Update the modal's content.
+  var modalTitle = exampleModal.querySelector('.modal-title')
+  var modalBodyInput = exampleModal.querySelector('.modal-body input')
+
+  modalTitle.textContent = 'Gửi đóng góp đến cho ' + recipient
+  modalBodyInput.value = recipient
+})
+
+// Thông báo gửi đóng góp thành công
+var alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+var alertTrigger = document.getElementById('liveAlertBtn')
+
+function alert(message, type) {
+  var wrapper = document.createElement('div')
+  wrapper.innerHTML = '<div class="alert alert-' + type + ' alert-dismissible" role="alert">' + message + '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>'
+
+  alertPlaceholder.append(wrapper)
+}
+
+if (alertTrigger) {
+  alertTrigger.addEventListener('click', function () {
+    alert('Cảm ơn bạn vì những đóng góp hữu ích ^^!!!', 'success')
+  })
+}
