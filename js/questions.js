b51664a78a63aedddfd570901e0ccf12d0576ab2
Hoàng Chu Anh Vũ <bonneybonny30082002@gmail.com>
2022-02-13 20:47:01 +0700

    Update Quizzes_COVID19 -- By VuHwang

---
+++ js/questions.js
@@ -0,0 +1,60 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Số thời gian trung bình mà bạn rời khỏi nhà trong một ngày là bao nhiêu?",
+    answer2: "Trong khoảng từ 30 phút tới 2 tiếng",
+    answer3: "Trong khoảng từ 2 tiếng tới 4 tiếng",
+    answer4: "Nhiều hơn 4 tiếng",
+    answer1: "Tôi không bao giờ ra khỏi nhà",
+    options: [
+      "Trong khoảng từ 30 phút tới 2 tiếng",
+      "Trong khoảng từ 2 tiếng tới 4 tiếng",
+      "Nhiều hơn 4 tiếng",
+      "Tôi không bao giờ ra khỏi nhà"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer1: "Common Style Sheet",
+    answer2: "Colorful Style Sheet",
+    answer3: "Computer Style Sheet",
+    answer4: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer1: "Hypertext Preprocessor",
+    answer2: "Hypertext Programming",
+    answer3: "Hypertext Preprogramming",
+    answer4: "Hometext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+  
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
