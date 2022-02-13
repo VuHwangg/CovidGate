7b8d31b7ac8a75276b6f2501a710823e513e1e20
unknown <nguyennghia22hd@gmail.com>
2022-02-13 05:09:57 -0800

    by ??

--- js/script.js
+++ js/script.js
@@ -8,6 +8,25 @@ window.addEventListener("scroll", ()=>{
     nav.classList.remove("nav-sticky")
   }
 });
+table{
+  border:1px solid black;
+}
+th{
+  border:1px solid black;
+}
+td{
+  border:1px solid black;
+}
+table, th, td{
+  border:1px solid #ccc;
+}
+table{
+  border-collapse:collapse;
+}
+tr:hover{
+  background-color:#ddd;
+  cursor:pointer;
+}
 
 
 // Toasts - Cửa sổ pop up
@@ -60,5 +79,3 @@ if (alertTrigger) {
 var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
   return new bootstrap.Tooltip(tooltipTriggerEl)
-})
-
